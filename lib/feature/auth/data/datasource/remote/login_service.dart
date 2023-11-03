4374d4ad34bc870c1253389d58aa2459506c913a
amir <amir.yazdanmanesh@gmail.com>
2023-11-03 19:21:22 +0330

    Add Login service

---
+++ lib/feature/auth/data/datasource/remote/login_service.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/dio.dart';
+import 'package:retrofit/http.dart';
+
+import '../../../../../core/constanst/general.dart';
+import '../../model/login_request.dart';
+import '../../model/login_response.dart';
+import '../../model/submit_code_request.dart';
+import '../../model/submit_code_response.dart';
+
+part 'login_service.g.dart';
+
+@RestApi(baseUrl: GeneralConstants.kBaseUrl)
+abstract class LoginService {
+  factory LoginService(Dio dio, {String baseUrl}) = _LoginService;
+
+  @POST('users/auth/xxx')
+  Future<HttpResponse<LoginResponse>> submitPhone(
+      @Body() LoginRequest loginRequest);
+
+  @POST("users/auth/xxx")
+  Future<HttpResponse<SubmitCodeResponse>> submitCode(
+      @Body() SubmitCodeRequest submitCodeRequest);
+}
